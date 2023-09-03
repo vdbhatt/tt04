eadaa26148e8e6a3d75d5fab5d064d6e9919ca5c
vijay deep bhatt T480 <vijaydeepbhatt@gmail.com>
2023-09-03 20:36:41 +0200

    tested locally, fixed typos and renamed things as required by flow

--- src/tb.v
+++ src/tb.v
@@ -28,7 +28,7 @@ module tb ();
     wire [7:0] uio_out;
     wire [7:0] uio_oe;
 
-    tt_vbhatt_uart_led tt_vbhatt_uart_led (
+    tt_um_vbhatt_uart_led tt_um_vbhatt_uart_led_dut (
     // include power ports for the Gate Level test
     `ifdef GL_TEST
         .VPWR( 1'b1),
--- src/tt_um_vbhatt_uart_led.v
+++ src/tt_um_vbhatt_uart_led.v
@@ -1,7 +1,7 @@
 `default_nettype none
 
 // module tt_um_seven_segment_seconds #( parameter MAX_COUNT = 24'd10_000_000 ) (
-module tt_vbhatt_uart_led #( parameter MAX_COUNT = 24'd10_000_000 ) (
+module tt_um_vbhatt_uart_led #( parameter MAX_COUNT = 24'd10_000_000 ) (
     input  wire [7:0] ui_in,    // Dedicated inputs - connected to the input switches
     output wire [7:0] uo_out,   // Dedicated outputs - connected to the 7 segment display
     input  wire [7:0] uio_in,   // IOs: Bidirectional Input path
@@ -13,10 +13,10 @@ module tt_vbhatt_uart_led #( parameter MAX_COUNT = 24'd10_000_000 ) (
 );
     wire rst= ~rst_n;
     top dut(
-        .i(uin_in[0]),
+        .i(ui_in[0]),
         .o(uo_out[0]),
         .clk(clk),
         .rst(rst),
-        .led_port(uo_out[6:1])
+        .led_port(uo_out[7:1])
         );
 endmodule
